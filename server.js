5ada8e621cb8ea121f3aece939f1225dc2c4ddec
Mahmoud Saadeh <mahmoud.saadeh998@gmail.com>
2021-05-31 15:18:37 +0300

    added dataBase

--- server.js
+++ server.js
@@ -1,24 +1,100 @@
 'use strict';
 
-require('dotenv').config();
 const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const jwksClient = require('jwks-rsa');
-
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
+mongoose.connect('mongodb://localhost:27017/test', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.get('/test', (request, response) => {
+const PORT = process.env.PORT;
 
-  // TODO: 
-  // STEP 1: get the jwt from the headers
-  // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
-  // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
-  // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
+const bookSchema = mongoose.Schema({
+  bookName: String,
+  describtion: String,
+  image: String,
+});
 
-})
+const Book = mongoose.model('book', bookSchema);
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+const userSchema = mongoose.Schema({
+  email: String,
+  books: [bookSchema],
+});
+
+const User = mongoose.model('user', userSchema);
+
+function createUser() {
+  const mahmoud = new User({
+    email: 'mahmoud.saadeh998@gmail.com',
+    books: [
+      {
+        bookName: 'Reinforcement Concrete',
+        describtion: 'talk about designing structural concrete',
+        image:
+          'https://images-na.ssl-images-amazon.com/images/I/81Wj1JQ+mKL.jpg',
+      },
+      {
+        bookName: 'Crime and Punishment',
+        describtion:
+          'Crime and Punishment focuses on the mental anguish and moral dilemmas of Rodion Raskolnikov, an impoverished ex-student in Saint Petersburg who formulates a plan to kill an unscrupulous pawnbroker for her money.',
+        image:
+          'https://kbimages1-a.akamaihd.net/b1c96137-0ddf-4ee4-8f46-73bdfa9b8621/1200/1200/False/crime-and-punishment-by-fyodor-dostoevsky-1.jpg',
+      },
+      {
+        bookName: 'Demons',
+        describtion:
+          'Demons is an allegory of the potentially catastrophic consequences of the political and moral nihilism that were becoming prevalent in Russia in the 1860s. A fictional town descends into chaos as it becomes the focal point of an attempted revolution, orchestrated by master conspirator Pyotr Verkhovensky.',
+        image:
+          'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1524586008l/5695.jpg',
+      },
+    ],
+  });
+  const alaa = new User({
+    email: 'alaaabuissa95@gmail.com',
+    books: [
+      {
+        bookName: 'Static',
+        describtion: 'talk about designing structural concrete',
+        image:
+          'https://images-na.ssl-images-amazon.com/images/I/81Wj1JQ+mKL.jpg',
+      },
+      {
+        bookName: 'Crime and Punishment2',
+        describtion:
+          'Crime and Punishment focuses on the mental anguish and moral dilemmas of Rodion Raskolnikov, an impoverished ex-student in Saint Petersburg who formulates a plan to kill an unscrupulous pawnbroker for her money.',
+        image:
+          'https://kbimages1-a.akamaihd.net/b1c96137-0ddf-4ee4-8f46-73bdfa9b8621/1200/1200/False/crime-and-punishment-by-fyodor-dostoevsky-1.jpg',
+      },
+      {
+        bookName: 'Demons',
+        describtion:
+          'Demons is an allegory of the potentially catastrophic consequences of the political and moral nihilism that were becoming prevalent in Russia in the 1860s. A fictional town descends into chaos as it becomes the focal point of an attempted revolution, orchestrated by master conspirator Pyotr Verkhovensky.',
+        image:
+          'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1524586008l/5695.jpg',
+      },
+    ],
+  });
+  alaa.save();
+  mahmoud.save();
+}
+// createUser();
+
+app.get('/books', (req, res) => {
+  const email = req.query.userEmail;
+
+  User.find({ email: email }, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.status(200).send(data[0].books);
+    }
+  });
+});
+
+app.listen(PORT, () => console.log(`listening on hi ${PORT}`));
